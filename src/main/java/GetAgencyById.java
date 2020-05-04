c88ab5f116f24ee2c30375b1c194ad48b1b8a4ab
Aland <aland.chang@smoothstack.com>
2020-05-04 17:25:06 -0400

    changed max connection size

--- src/main/java/GetAgencyById.java
+++ src/main/java/GetAgencyById.java
@@ -19,24 +19,24 @@ public class GetAgencyById implements RequestHandler<ApiGatewayRequest, ApiGatew
 
 	public ApiGatewayProxyResponse handleRequest(ApiGatewayRequest request, Context context) {
 		LambdaLogger logger = context.getLogger();
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put("Access-Control-Allow-Origin", "*");
 		try {
 			if (request.getPathParameters() == null || request.getPathParameters().get("agencyId") == null) {
-				return new ApiGatewayProxyResponse(400, null, null);
+				return new ApiGatewayProxyResponse(400, headers, null);
 			}
 			Agency agency = agentService.getAgencyById(Long.parseLong(request.getPathParameters().get("agencyId")));
 			if (agency == null) {
-				return new ApiGatewayProxyResponse(404, null, null);
+				return new ApiGatewayProxyResponse(404, headers, null);
 			}
-			Map<String, String> headers = new HashMap<String, String>();
-			headers.put("Access-Control-Allow-Origin", "*");
 			return new ApiGatewayProxyResponse(200, headers, new Gson().toJson(agency));
 
 		} catch (NumberFormatException | SQLException e) {
 			logger.log(e.getMessage());
-			return new ApiGatewayProxyResponse(400, null, null);
+			return new ApiGatewayProxyResponse(400, headers, null);
 		} catch (ClassNotFoundException e) {
 			logger.log(e.getMessage());
-			return new ApiGatewayProxyResponse(500, null, null);
+			return new ApiGatewayProxyResponse(500, headers, null);
 		}
 	}
 }
